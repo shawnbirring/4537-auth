749074e6d8881aa74a0165ba288604f19ef567e4
shawnbirring <shawnbirring21@gmail.com>
2024-03-18 11:51:30 -0700

    completed auth functions and added user scheme

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(session
+    ({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false }
+    }));
+
+app.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword
+            }
+        });
+        res.status(201).json({ message: `User created successfully: ${email}` });
+    } catch (error) {
+        res.status(500).json({ message: `An error occurred while registering user: ${email}` });
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            return res.status(404).json({ error: "User Not found" })
+        }
+
+        const passwordMatch = bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+        req.session.userID = user.id;
+        res.status(200).json({ message: "Login Successful" });
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while loggin in" });
+    }
+})
+
+app.get("/admin", async (req, res) => {
+    try {
+        const isAdmin = await prisma.user.findFirst({
+            where: {
+                id: req.session.userID,
+                isAdmin: true
+            }
+        });
+        if (!isAdmin) {
+            return res.status(403).json({ error: 'Forbidden' });
+        }
+        res.status(200).json({ message: 'Admin page', apiCalls: isAdmin.apiCalls });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while accessing admin page' });
+    }
+})
+
+app.get('/user', async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: req.session.userId }
+        });
+        res.status(200).json({ message: 'User landing page', apiCalls: user.apiCalls });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while accessing user page' });
+    }
+});
+
+app.get('/logout', async (req, res) => {
+    try {
+        req.session.destroy((err) => {
+            if (err) {
+                return res.status(500).json({ error: 'An error occurred while logging out' });
+            }
+            res.clearCookie('connect.sid');
+            res.status(200).json({ message: 'Logout successful' });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while logging out' });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
